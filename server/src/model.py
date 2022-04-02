f50d73ee761f0822450ffdd376e41d98060c0b61
chalonhutson <chalonhutson@gmail.com>
2022-04-02 11:01:40 -0600

    Added viewer and message table.

--- server/src/model.py
+++ server/src/model.py
@@ -14,6 +14,20 @@ class Admin(db.Model):
     def __repr__(self):
         return f"<User id: {self.id}, username: {self.username}>"
 
+class Viewer(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    twitch_name = db.Column(db.String(80), unique=True, nullable=False)
+    timezone = db.Column(db.Integer, nullable=True)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    messages = db.relationship("Chat_message", backref='viewer', lazy=True)
+
+
+class Chat_message(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    viewer_id = db.Column(db.Integer, db.ForeignKey("viewer.id"), nullable=False)
+    message_content(db.String, nullable=False)
+    message_datetime(db.Datetime, nullable=False)
+
 def connect_to_db(app):
     db.app = app
     db.init_app(app)
