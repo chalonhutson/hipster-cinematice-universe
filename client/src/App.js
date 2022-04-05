eaa6e401f8129fc99b85afc5059c4ff28270dae1
chalonhutson <chalonhutson@gmail.com>
2022-04-04 19:13:59 -0600

    Began adding another page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
-import LoginRegister from './components/LoginRegister';
+import { useState } from 'react'
+import './App.css'
+import LoginRegister from './components/LoginRegister'
+import ViewerStats from './components/ViewerStats'
 
 function App() {
 
+  const [page, setPage] = useState("LoginRegister")
 
   return (
     <div className="App">
-      <LoginRegister />
+      <button onClick={() => setPage("LoginRegister")}>Login Register Page</button>
+      <button onClick={() => setPage("ViewerStats")}>Viewer Stats</button>
+      { page === "LoginRegister" ? <LoginRegister /> : null }
+      { page === "ViewerStats" ? <ViewerStats /> : null }
     </div>
   );
 }
---
+++ client/src/components/ViewerStats.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ViewerStats() {
+  return (
+    <div>
+        Viewer Stats
+    </div>
+  )
+}
