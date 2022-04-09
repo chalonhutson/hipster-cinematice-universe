240623ab8a44c90609aa8e024358619c331ab45b
chalonhutson <chalonhutson@gmail.com>
2022-04-09 13:16:34 -0600

    Added component to view messages.

--- client/src/components/ViewerStats.js
+++ client/src/components/ViewerStats.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function ViewerStats() {
+
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    fetch("/get_messages/1").then(res => res.json()).then(data => {
+      setMessages(data)
+    })
+  }, [])
+
+  const deliveredMessages = messages.map((i) => {
+    return (
+    <div key={i.message_id}>
+      <h4 key={`Message ${i.message_id.toString()}`}>Message: {i.message_content}</h4>
+      <h4 key={`Datetime ${i.message_id.toString()}`}>Message Date: {i.message_datetime}</h4>
+    </div>
+    )
+  })
+
   return (
     <div>
-        Viewer Stats
+        <h1>Viewer Stats</h1>
+        <div>
+          <h2>Messages</h2>
+
+          { deliveredMessages }
+          
+        </div>
     </div>
   )
 }
